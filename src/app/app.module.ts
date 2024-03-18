e11fc2c91c8cc8de7721f828e23ba9efa9654f59
suryansh <suryanshchaudhary908@gmail.com>
2024-03-18 16:20:53 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './NavBar/nav-bar/nav-bar.component';
+import { HomeComponent } from './Home/home/home.component';
+import { MostLikedProductsComponent } from './most-liked-products/most-liked-products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, NavBarComponent, HomeComponent, MostLikedProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/most-liked-products/most-liked-products.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-most-liked-products',
+  templateUrl: './most-liked-products.component.html',
+  styleUrls: ['./most-liked-products.component.css']
+})
+export class MostLikedProductsComponent {
+
+}
